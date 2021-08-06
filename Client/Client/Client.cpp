8ab6856f1d8674346347a32f71582097b48f69df
tqthinh16 <thaiduong161@gmail.com>
2021-08-06 16:03:02 +0700

    Update Client.cpp

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -24,6 +24,7 @@ int main() {
 	cout << "Enter IP: ";
 	getline(cin, IP);
 	string username, password;
+	string input;
 
 	// Initialize WinSock
 	WSAData data;
@@ -49,32 +50,71 @@ int main() {
 		cout << "connect failed" << endl;
 	}
 
+	cout << "Connected!" << endl;
 
-TRYAGAIN:
-	cout << "Username: ";
-	getline(cin, username);
-	cout << "password: ";
-	getline(cin, password);
-	send(sock, username.c_str(), username.size() + 1, 0);
-	send(sock, password.c_str(), password.size() + 1, 0);
+MENU:
+	cout << "1. Login" << endl << "2. Sign up" << endl << "0. Exit" << endl << "Choose function: ";
+	
+	getline(cin, input);
 
-	recv(sock, buffer, 1024, 0);
-	if (strcmp(buffer, "failed") == 0) {
-		cout << "Wrong username or password, pls try again!" << endl;
-		goto TRYAGAIN;
+	send(sock, input.c_str(), 2, 0);
+
+	if (input == "1") {
+
+	TRYAGAIN1:
+		cout << "LOGIN" << endl;
+		cout << "Username: ";
+		getline(cin, username);
+		cout << "password: ";
+		getline(cin, password);
+		send(sock, username.c_str(), username.size() + 1, 0);
+		send(sock, password.c_str(), password.size() + 1, 0);
+
+		recv(sock, buffer, 1024, 0);
+		if (strcmp(buffer, "failed") == 0) {
+			cout << "Wrong username or password, pls try again!" << endl;
+			goto TRYAGAIN1;
+		}
+	}
+
+	else if (input == "2") {
+	TRYAGAIN2:
+		cout << "REGISTER" << endl;
+		cout << "Username: ";
+		getline(cin, username);
+		cout << "password: ";
+		getline(cin, password);
+		send(sock, username.c_str(), username.size() + 1, 0);
+		send(sock, password.c_str(), password.size() + 1, 0);
+
+		recv(sock, buffer, 1024, 0);
+
+		if (strcmp(buffer, "failed") == 0) {
+			cout << "username is not available, pls try again!" << endl;
+			goto TRYAGAIN2;
+		}	
+
+		goto MENU;
+
+	}
+	else if (input == "0") {
+		cout << "close connection" << endl;
+		exit(1);
 	}
 
 	string clientInput;
 
 	/// <summary>
-	/// ////////////////////////////
-	/// </summary>
-	/// <returns></returns>
 	do
 	{
 		// Prompt the user for some text
-		cout << "> ";
+		cout << "Enter province's name for look up (0. Exit): ";
 		getline(cin, clientInput);
+
+		if (clientInput == "0") {
+			cout << "Exit" << endl;
+			exit(1);
+		}
 
 		if (clientInput.size() > 0)		// Make sure the user has typed in something
 		{
@@ -84,6 +124,7 @@ TRYAGAIN:
 			{
 				// Wait for response
 				ZeroMemory(buffer, 1024);
+
 				int bytesReceived = recv(sock, buffer, 1024, 0);
 				if (bytesReceived > 0)
 				{
