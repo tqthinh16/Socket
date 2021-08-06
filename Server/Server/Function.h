04428d48a042fbca7c01a018fc9fd151fd785a4b
tqthinh16 <thaiduong161@gmail.com>
2021-08-06 16:05:32 +0700

    Update Function.h

--- Server/Server/Function.h
+++ Server/Server/Function.h
@@ -9,20 +9,40 @@
 using namespace std;
 
 
-struct Node {
+struct Client {
 	string username;
 	string password;
-	Node* next;
+	Client* next;
 };
 
-struct LoginList {
-	Node* head;
-	Node* tail;
+struct ClientList {
+	Client* head = nullptr;
+	Client* tail = nullptr;
 };
 
-Node* create1(string str);
-void getLoginData(LoginList& l, string file_name);
-bool checkAvailableUsername(LoginList& l, string user);
-Node* create2(string username, string password);
-void UserReg(LoginList& l, string user, string pass);
-bool checkCorrect(LoginList& l, string user, string pass);
+struct Province {
+	string name;
+	int Cases;
+	int uTreatment;
+	int Recovery;
+	int Death;
+	int Other;
+	Province* next = nullptr;
+};
+
+struct ProvinceList {
+	Province* head = nullptr;
+	Province* tail = nullptr;
+};
+
+
+Client* create1(string str);
+void getLoginData(ClientList& l, string file_name);
+bool checkAvailableUsername(ClientList& l, string user);
+Client* create2(string username, string password);
+void UserReg(ClientList& l, string user, string pass);
+bool checkCorrect(ClientList& l, string user, string pass);
+Province* create3(string str);
+void getProvinceData(ProvinceList& l, string file_name);
+Province* findProvince(ProvinceList& l, string name);
+
